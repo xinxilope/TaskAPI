ee2c4bf7e70cc0a52adc9fd1216a34029053c231
xinxilope <mateuseidt10@gmail.com>
2022-10-18 18:05:01 -0300

    find post by id

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ class Post(BaseModel):
 
 
 
-my_posts = []
+my_posts = [{"titulo":"aaaa","content":"amamammama","id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 
 
 
@@ -37,3 +43,8 @@ def create_posts(post: Post):
     post_dict['id'] = randrange(0,100000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return{"post":post}
