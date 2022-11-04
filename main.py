80cde687213a333fbc93e17ef6a960310935a521
xinxilope <mateuseidt10@gmail.com>
2022-11-04 16:03:48 -0300

    Deleting post

--- main.py
+++ main.py
@@ -30,6 +30,11 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 
 
 
@@ -56,3 +61,11 @@ def get_post(id: int):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return{"post":post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    my_posts.pop(index)
+    return {}
