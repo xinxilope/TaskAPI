6afb685833b15c2b3116af29602f9fba4cbb3ece
xinxilope <mateuseidt10@gmail.com>
2023-02-15 15:00:10 -0300

    feat: response chemas

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import pyodbc, os, time
 from . import models, schemas
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -28,25 +29,25 @@ while True:
 def root():
     return {"message": "Welcome to Home Page!"}
 
-@app.get("/posts")
+@app.get("/posts", response_model=list[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.POS_ID == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return{"data":post}
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -57,7 +58,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
+@app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.POS_ID == id)
     post = post_query.first()
@@ -65,5 +66,5 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return{"data":post_query.first()}
+    return post_query.first()
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,20 @@
+from typing import Optional
 from pydantic import BaseModel
-
+from datetime import datetime
 
 
 class PostBase(BaseModel):
     POS_TITLE: str
-    POS_DESCRIPTION: str
+    POS_DESCRIPTION: Optional[str]
     POS_PUBLISHED: bool = True
 
 
 class PostCreate(PostBase):
     pass
+
+
+class Post(PostBase):
+    POS_ID: int
+    POS_CREATED_AT: datetime
+    class Config:
+        orm_mode = True
