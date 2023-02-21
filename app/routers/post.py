5948816c2c77a8f0872f642810a1a40e0263a46c
xinxilope <mateuseidt10@gmail.com>
2023-02-21 15:36:46 -0300

    fix: deleting and updating individual posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -40,11 +40,16 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-    post = db.query(models.Post).filter(models.Post.POS_ID == id)
+    post_query = db.query(models.Post).filter(models.Post.POS_ID == id)
+    post = post_query.first()
 
-    if post.first() == None:
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    post.delete(synchronize_session=False)
+
+    if post.POS_USU_ID != current_user.USU_ID:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -57,6 +62,10 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+    if post.POS_USU_ID != current_user.USU_ID:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
 
